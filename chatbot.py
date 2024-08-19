5636934f917a60344091428af4f82b996991cd25
Maeve Ho <157730021+ngh227@users.noreply.github.com>
2024-08-18 21:56:01 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -92,7 +92,6 @@ def generate_response(user_input, context):
         inputs = tokenizer(user_input, context, return_tensors = "pt", max_length=512, truncation=True)
         with torch.no_grad():
             outputs = model(**inputs)
-######## WHERE I MODIFIED ######### 
         answer_start = torch.argmax(outputs.start_logits)
         answer_end = torch.argmax(outputs.end_logits) + 1
         answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
